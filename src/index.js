50c32e663ca5f7a680f484ee522083160e3f1c1b
Kaloyan168 <pvoivoda183@gmail.com>
2025-01-31 20:48:32 +0200

    addAndSetUpHandlebars

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Ewala');
+import express from 'express';
+import handlebars from 'express-handlebars'
+
+const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}))
+app.set('view engine', 'hbs');
+app.set('views', './src/views')
+
+
+app.get('/', (req, res) => {
+    res.render('home', {layout: false})
+});
+
+app.listen(5000, () => console.log('http://localhost:5000...'))
