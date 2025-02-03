206512966be4343cc1907ac676fbbef815c1ff19
Kaloyan168 <pvoivoda183@gmail.com>
2025-02-03 19:29:22 +0200

    dynamicAction

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,12 @@
 export default [
     {
+    id: '1',
     title: 'Jungle Cuise',
     genre: 'Adventure',
     description: "Descrizption: Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.",
     imageUrl: './static/img/jungle-cruise.jpeg',
 },{
+    id: '2',
     title: 'Squid Game 2',
     genre: 'Drama',
     description: 'Hundreds of cash-strapped players accept a strange invitation to compete in childrens games. Inside, a tempting prize awaits with deadly high stakes: a survival game that has a whopping 45.6 billion-won prize at stake.',
