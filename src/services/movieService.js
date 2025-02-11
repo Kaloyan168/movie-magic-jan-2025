f081f04a4d683f567ba095366abf5eebc7191109
Kaloyan168 <pvoivoda183@gmail.com>
2025-02-11 20:27:43 +0200

    addCreateCast

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,34 +1,35 @@
-import Movie from '../models/Movie.js'
+import Movie from '../models/Movie.js';
 
 export default {
     getAll(filter = {}){
         let query = Movie.find({});
-        // TODO: fix partial case insensitive search
         if(filter.search){
-            query = query.where({title: filter.search})
+            // TODO fix partial case insensitive search
+            query = query.where({title: filter.search});
         };
-        //TODO: add case insensitive search
+
         if(filter.genre) {
-            query = query.where({genre: filter.genre})
+            // TODO: add case insensitive search
+            query = query.where({genre: filter.genre});
         }
 
         if(filter.year) {
-            query = query.where({year: Number(filter.year)})
+            query = query.where({year: Number(filter.year)});
         }
         return query;
     },
     getOne(movieId) {
-        const result = Movie.findById(movieId);
+        const result = Movie.findById(movieId)
 
         return result;
     },
     create(movieData){
-        const result = Movie.create(movieData)({
+        const result  = Movie.create({
             ...movieData,
-            rating: Number(movieData.rating),
-            year: Number(movieData.year)
-        }
-        );
+            rating: Number(movieData.rating),  
+            year: Number(movieData.year)   
+        });
+        
         return result;
     }
 }
