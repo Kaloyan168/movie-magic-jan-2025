b9be62e05c6ee7f37441f47bc690d91f9ab9fb0c
Kaloyan168 <pvoivoda183@gmail.com>
2025-02-11 17:58:18 +0200

    RefactorHomePageToUseDb

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,32 +1,38 @@
+import { v4 as uuid } from 'uuid';
 import movies from '../movies.js'
-import { v4 as uuid } from 'uuid'
+import Movie from '../models/Movie.js'
 
 export default {
     getAll(filter = {}){
-        let result = movies;
-        if(filter.search){
-            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))
-        }
-        if (filter.genre) {
-            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase())
-        }
-        if (filter.year) {
-            result = result.filter(movie => movie.year === filter.year)
-        }
-        return result;
+        // let result = Movie.find({});
+        // if(filter.search){
+        //     result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
+        // };
+
+        // if(filter.genre) {
+        //     result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase());
+        // }
+
+        // if(filter.year) {
+        //     result = result.filter(movie => movie.year === filter.year);
+        // }
+        return Movie.find({});
     },
-    findOne(movieId){
-        const result = movies.find(movie => movie.id === movieId);
+    getOne(movieId) {
+        const result = Movie.findById(movieId);
 
         return result;
-    }, create(movieData){
-        // todo: add ids
-        const newId = uuid();
-        movies.push({
-            id: newId, 
+    },
+    create(movieData){
+        // TODO: add IDs
+        const newId = uuid;
+        movies.push(
+            {id: newId,
             ...movieData,
-            rating: Number(movieData.rating)
-        })
+            rating: Number(movieData.rating),
+            
+        }
+        );
         return newId;
     }
 }
